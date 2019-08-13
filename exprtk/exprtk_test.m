b945655569ddee54e4e4b894be4b38baf7ed27bd
Denis von Arx <dvarx@gmx.ch>
2019-08-13 22:06:59 +0200

    added error handling

--- exprtk/exprtk_test.m
+++ exprtk/exprtk_test.m
@@ -8,5 +8,11 @@ ts=linspace(0,10,1000);
 expression='switch{case t<1: 1;case t<2: 2; default: 5*sin(t)*exp(-t);}';
 ys=exprtk_eval(expression,ts);
 
+if any(isnan(ys))
+    fprintf("Warning: Expression could not be evaluated for certain t.\n");
+end
+
 plot(ts,ys);
 grid();
+ylabel("Expression expr(t)");
+xlabel("Time t [s]");
